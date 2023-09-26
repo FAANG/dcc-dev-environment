a542aedbebe9d09fd38f2026378af22809c28361
Mil-m <mansurova.m94@gmail.com>
2023-09-26 16:10:48 +0100

    range of ports in K8s service

--- dcc-dev/build.sh
+++ dcc-dev/build.sh
@@ -7,4 +7,5 @@ docker build -t dcc-dev-img:latest --no-cache .
 docker tag dcc-dev-img:latest milm/dcc-dev-img:latest
 docker push milm/dcc-dev-img:latest
 
+kubectl delete -f ssh-service.yaml
 kubectl apply -f ssh-service.yaml
